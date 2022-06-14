000562eb09b87e9417322182f537f44d174b9cb6
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2022-06-14 22:07:56 +0300

    Basic placement and Entry Generator

---
+++ Assets/LeaderboardScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateEntries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Create up to 2000 new entries for the leaderboard
+    void GenerateEntries()
+    {
+        GameObject entry = transform.Find("Entry").gameObject;
+        GameObject newEntry=Instantiate(entry,gameObject.transform);
+        newEntry.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, entry.GetComponent<RectTransform>().anchoredPosition.y- 50f);
+    }
+}
