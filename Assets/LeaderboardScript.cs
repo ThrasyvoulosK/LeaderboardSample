c0f214c4246ba5d1dfcd056a8253f292bc27a0b1
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2022-06-16 20:40:36 +0300

    Entry Generator Improvements and text overflow

--- Assets/LeaderboardScript.cs
+++ Assets/LeaderboardScript.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
 public class LeaderboardScript : MonoBehaviour
 {
+    public int originalScore = 1000000;
+    public int numberOfEntries = 2000;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +24,21 @@ public class LeaderboardScript : MonoBehaviour
     //Create up to 2000 new entries for the leaderboard
     void GenerateEntries()
     {
-        for (int i = 1; i <= 50; i++)
+        for (int i = 1; i <= numberOfEntries; i++)
         {
             GameObject entry = transform.Find("EntriesBg").Find("Entries").Find("Entry").gameObject;
             GameObject newEntry = Instantiate(entry, entry.transform.parent);
+            
+            //Set new entry's position relatively to the original
             newEntry.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, entry.GetComponent<RectTransform>().anchoredPosition.y - i*30f);
+
+            //Assign its attributes
+            //1.Position
+            newEntry.transform.Find("Position").GetComponent<TextMeshProUGUI>().text = (i + 1).ToString();
+            //2.Name
+            newEntry.transform.Find("Name").GetComponent<TextMeshProUGUI>().text ="name"+(i + 1).ToString();
+            //3.Score
+            newEntry.transform.Find("Score").GetComponent<TextMeshProUGUI>().text =(originalScore-i*(originalScore/numberOfEntries)).ToString();
         }
     }
 }
